2d14693534e38d252da1dee4c6ad9e93e940f760
JamesMReilly <jimreilly28@gmail.com>
2018-01-24 16:15:45 -0600

    initial commit

---
+++ outofclass-practice/frenchflag.js
@@ -0,0 +1,32 @@
+var bluestripe = { "x_axis": 10, "y_axis": 10, "height": 90, "width": 40, "color": "blue" }
+var whitestripe = { "x_axis": 50, "y_axis": 10, "height": 90, "width": 40, "color": "white" }
+var redstripe = { "x_axis": 90, "y_axis": 10, "height": 90, "width": 40, "color": "red" }
+
+var flag = [bluestripe, whitestripe, redstripe];
+
+var max_x = 0;
+var max_y = 0;
+
+for (var i = 0; i < flag.length; i++) {
+  var temp_x = flag[i].x_axis + flag[i].width;
+  var temp_y = flag[i].y_axis + flag[i].height;
+
+  if (temp_x > max_x) max_x = temp_x;
+  if (temp_y > max_y) max_y = temp_y;
+}
+
+var svg = d3.select("body").append("svg")
+  .attr("width", max_x + 10)
+  .attr("height", max_y + 10)
+
+var rectangles = svg.selectAll("rect")
+  .data(flag)
+  .enter()
+  .append("rect")
+
+var rectAttr = rectangles
+  .attr("x", function (d) { return d.x_axis; })
+  .attr("y", function (d) { return d.y_axis; })
+  .attr("height", function (d) { return d.height; })
+  .attr("width", function (d) { return d.width; })
+  .attr("fill", function (d) { return d.color; })
